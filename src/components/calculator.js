8cf0c6c0a8bbc67bb1ebec1104e01f4847dd4890
kaizipaul <kaizipaul@gmail.com>
2022-10-28 16:42:45 +0300

    Corrections per code review

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,26 +7,28 @@ class calculator extends React.Component {
       <div className="calculator-grid">
         <div className="output">
           <div data-previous-operand className="previous-operand" />
-          <div data-current-operand className="current-operand" />
+          <div data-current-operand className="current-operand">0</div>
         </div>
-        <button type="button" data-all-clear className="span-two">AC</button>
-        <button type="button" data-delete>DEL</button>
-        <button type="button" data-operation>÷</button>
-        <button type="button" data-number>1</button>
-        <button type="button" data-number>2</button>
-        <button type="button" data-number>3</button>
-        <button type="button" data-operation>*</button>
-        <button type="button" data-number>4</button>
-        <button type="button" data-number>5</button>
-        <button type="button" data-number>6</button>
-        <button type="button" data-operation>+</button>
+        <button type="button" data-all-clear>AC</button>
+        <button type="button" data-plusminus>±</button>
+        <button type="button" data-percent>%</button>
+        <button type="button" className="operation" data-operation>÷</button>
         <button type="button" data-number>7</button>
         <button type="button" data-number>8</button>
         <button type="button" data-number>9</button>
-        <button type="button" data-operation>-</button>
+        <button type="button" className="operation" data-operation>x</button>
+        <button type="button" data-number>4</button>
+        <button type="button" data-number>5</button>
+        <button type="button" data-number>6</button>
+        <button type="button" className="operation" data-operation>-</button>
+        <button type="button" data-number>1</button>
+        <button type="button" data-number>2</button>
+        <button type="button" data-number>3</button>
+        <button type="button" className="operation" data-operation>+</button>
+        <button type="button" data-number className="span-two">0</button>
         <button type="button" data-number>.</button>
-        <button type="button" data-number>0</button>
-        <button type="button" data-equals className="span-two">=</button>
+
+        <button type="button" className="operation" data-equals>=</button>
       </div>
     );
   }
