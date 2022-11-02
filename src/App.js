e4076247f9ea2ba3b7619e24a3eb1c0ba0df9f7a
kaizipaul <kaizipaul@gmail.com>
2022-11-02 12:32:38 +0300

    Created routes for the components and imported the new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 /* eslint-disable react/prefer-stateless-function */
 import './App.css';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import Calculator from './components/calculator';
+import Quotes from './components/Quotes';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/calculator" element={<Calculator />} />
+      <Route exact path="/quotes" element={<Quotes />} />
+    </Routes>
+  </div>
+);
 
 export default App;
